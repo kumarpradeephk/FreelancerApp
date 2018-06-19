a27ab0dbcdc998250c33ed2fc2c894f017d0bd8a
kumarpradeep <pradeep@f22labs.com>
2018-06-19 16:52:10 +0530

    updated production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   address: "smtp.gmail.com",
+   port: 587,
+   domain: ENV["DOMAIN_NAME"],
+   user_name: ENV["GMAIL_USERNAME"],
+   password: ENV["GMAIL_PASSWORD"],
+   authentication: "login",
+   enable_starttls_auto: true
+}
 end
